2e26c02b0db85359c485de8b7e563b5c8c123294
João Nascimento <joao.nascimento0576@gmail.com>
2021-10-23 16:01:50 -0300

    Criação de models de pessoa, funcionario e gerente

---
+++ src/app/models/Pessoa.ts
@@ -0,0 +1,15 @@
+export interface Pessoa {
+    id: string
+    nome: string
+    email: string
+    cpf: string
+    dataNasc: Date
+    telefone: string
+    rg: string
+    rua: string
+    bairro: string
+    numero: string
+    cep: string
+    cidade: string
+    estado: string
+}
