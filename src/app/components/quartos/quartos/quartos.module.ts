b967a752e533cfe39d9fbc95906fa60126ceb678
Joao Carlos Silva do Nascimento <joao.nascimento@concert.com.br>
2021-10-30 18:53:53 -0300

    Alteração nos estilos e componente de administração de quartos

--- src/app/components/quartos/quartos/quartos.module.ts
+++ src/app/components/quartos/quartos/quartos.module.ts
@@ -5,16 +5,20 @@ import { QuartosRoutingModule } from './quartos-routing.module';
 import { QuartosComponent } from '../quartos.component';
 import { CategoriaComponent } from '../categoria/categoria.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AdministrarQuartosComponent } from '../administrar-quartos/administrar-quartos.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     QuartosComponent,
     CategoriaComponent,
+    AdministrarQuartosComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    QuartosRoutingModule
+    QuartosRoutingModule,
+    FontAwesomeModule
   ]
 })
 export class QuartosModule { }
