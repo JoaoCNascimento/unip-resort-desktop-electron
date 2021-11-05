fb2686a4bcc9d47cf5a22fc9b7671fe700b2edf2
João Nascimento <joao.nascimento0576@gmail.com>
2021-11-05 15:23:37 -0300

    Criação de guard para rotas, interceptors para headers de requisições e pipes para formatação de visualização de dados

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/api/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(request);
+    
+    if (this.authService.token) {
+      request = request.clone({
+        setHeaders: {
+          'Authorization': this.authService.token,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
